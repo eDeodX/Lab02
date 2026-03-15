9b04e99aea397f6b04b5c3fc3c92a34e2d3da3c7
eDeodX <hanromik124@gmail.com>
2026-03-15 19:40:40 +0300

    add comments in hello_world.cpp

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,10 +1,10 @@
-#include <iostream>
+#include <iostream> // подключаем заголовочные файлы
 #include <string>
 
 int main() {
-	std::string name;
+	std::string name; // создаем переменную имени
 	std::cout << "Enter your name: ";
-	std::getline(cin, name);
-	std::cout << "Hello world from " << name << std::endl;
+	std::getline(cin, name); // используем geline, т.к обычный cin считывает только до пробела
+	std::cout << "Hello world from " << name << std::endl; // выводим результат в консоль
 	return 0;
 }
